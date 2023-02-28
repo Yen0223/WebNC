ed61feade842d7312207b63c996547ed10061486
Yen <yenln0223@gmail.com>
2023-03-01 03:18:26 +0700

    truy van

---
+++ src/Lab1/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Contexts;
+
+namespace TatBlog.Services.Blogs;
+
+public class BlogRepository : IBlogRepository
+{
+
+    private readonly BlogDbContext _context;
+
+    public BlogRepository(BlogDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Post> GetPostAsync(
+        int year,
+        int month,
+        string slug,
+        CancellationToken cancellationToken = default)
+    {
+        IQueryable<Post> postsQuery = _context.Set<Post>()
+            .Include(x => x.Category)
+            .Include(x => x.Author);
+        if (year > 0)
+        {
+            postsQuery = postsQuery.Where(x => x.PostedDate.Year == year);
+        }
+
+        if (month > 0)
+        {
+            postsQuery = postsQuery.Where(x => x.PostedDate.Month  == month);
+        }
+
+        if (!string.IsNullOrWhiteSpace(slug))
+        {
+            postsQuery = postsQuery.Where(x => x.UrlSlug == slug);
+        }
+
+        return await postsQuery.FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public async Task<IList<Post>> GetPopularArticlesAsync(
+        int numPosts, CancellationToken cancellationToken = default)
+    {
+        return await _context.Set<Post>()
+            .Include(x => x.Author)
+            .Include(x => x.Category)
+            .OrderByDescending(x => x.ViewCount)
+            .Take(numPosts)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<bool> IsPostSlugExistedAsync(
+        int postId,
+        string slug,
+        CancellationToken cancellationToken = default)
+    {
+        return await _context.Set<Post>()
+            .AnyAsync(x => x.Id != postId && x.UrlSlug == slug, 
+            cancellationToken);
+        
+            
+    }
+    public async Task IncreaseViewCountAsync(
+        int postId,
+        CancellationToken cancellationToken = default)
+    {
+        await _context.Set<Post>()
+            .Where(x => x.Id == postId)
+            .ExecuteUpdateAsync(p => p.SetProperty(x => x.ViewCount,
+             x => x.ViewCount + 1),
+             cancellationToken);
+    }
+
+}
--- src/Lab1/TipsAndTricks/TatBlog.WinApp/Program.cs
+++ src/Lab1/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -3,10 +3,11 @@
 
 using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
 
 var context = new BlogDbContext();
 
-var seeder = new DataSeeder(context);
+/*var seeder = new DataSeeder(context);
 
 seeder.Initialize();
 
@@ -20,4 +21,34 @@ foreach (var author in authors)
     Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}",
         author.Id, author.FullName, author.Email, author.JoinedDate);
 
+}
+*/
+
+/*var posts = context.Posts
+    .Where(p => p.Published)
+    .OrderBy(p => p.Title)
+    .Select(p => new
+    {
+        Id = p.Id,
+        Title = p.Title,
+        ViewCount = p.ViewCount,
+        PostedDate = p.PostedDate,
+        Author = p.Author.FullName,
+        Category = p.Category.Name,
+    })
+    .ToList();*/
+
+IBlogRepository blogRepo = new BlogRepository(context);
+
+var posts = await blogRepo.GetPopularArticlesAsync(3);
+
+foreach (var post in posts)
+{
+    Console.WriteLine("ID       : {0}", post.Id);
+    Console.WriteLine("Title    : {0}", post.Title);
+    Console.WriteLine("View     : {0}", post.ViewCount);
+    Console.WriteLine("Date     : {0:MM/dd/yyyy}", post.PostedDate);
+    Console.WriteLine("Author   : {0}", post.Author.FullName);
+    Console.WriteLine("Category :{0}", post.Category.Name);
+    Console.WriteLine("".PadRight(80, '-'));
 }
