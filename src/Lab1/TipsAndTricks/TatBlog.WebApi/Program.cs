c8f51d9feab9409558dbfa16d955df932a6260f8
Yen <yenln0223@gmail.com>
2023-03-28 15:52:51 +0700

    service webapi

--- src/Lab1/TipsAndTricks/TatBlog.WebApi/Program.cs
+++ src/Lab1/TipsAndTricks/TatBlog.WebApi/Program.cs
@@ -1,5 +1,19 @@
+using TatBlog.WebApi.Extensions;
+
+using TatBlog.WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder
+        .ConfigureCors()
+        .ConfigureNlog()
+        .ConfigureServices()
+        .ConfigureSwaggerOpenApi();
+}
 
 var app = builder.Build();
+{
+    app.SetupRequestPipeline();
 
-app.Run();
+    app.Run();
+}
